8ddb5abdee2a84a8dec9d3bd97e5bbcdd0c42d2f
2003parvshah <2003parv@gmail.com>
2024-05-18 18:45:13 +0530

    addToCart

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,8 +16,8 @@ class UserSeeder extends Seeder
     {
         //
         DB::table('users')->insert([
-            'name'=>'peter parker',
-            'email'=>'peter@parker.com',
+            'name'=>'parv shah',
+            'email'=>'2003parv@gmail.com',
             'password'=>Hash::make('12345')
         ]);
     }
